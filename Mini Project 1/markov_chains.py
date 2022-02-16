7523b07e24b58441875aa00d25ff542f4c4943b8
mathhauk <99803625+mathhauk@users.noreply.github.com>
2022-02-16 17:42:08 +0000

    Task 6 and 7

--- Mini Project 1/markov_chains.py
+++ Mini Project 1/markov_chains.py
@@ -1,6 +1,5 @@
 #A datastructure to encode DTMC (discrete-time Markov chanis)
-
-
+import re
 
 # Task 1
 # a class for encoding a DTMC from a .txt file
@@ -30,6 +29,12 @@ class probability_distribution:
 
     def __str__(self):
         return self.name
+    
+    def setProbabilities(self, probabilities):
+        self.probabilities = probabilities
+    
+    def getProbabilities(self):
+        return self.probabilities
 
 # Task 3
 class MarkovChain:
@@ -60,8 +65,83 @@ class MarkovChain:
                 f.write('\t' + dist.DTMC.states[i] + ': ' + str(dist.probabilities[i]) + ';\n')
             f.write('end \n \n')
         f.close
+
+
+# Task 6
+
+class Token:
+
+    def __init__(self, type, string, line, column):  
+        self.type = type
+        self.string = string
+        self.line = line
+        self.column = column
+
+    def setType(self, type):
+        self.type = type
+    
+    def getType(self):
+        return self.type
+
+    def setString(self, string):
+        self.string = string  
+          
+    def getString(self):
+        return self.string
+     
+    def setLine(self, line):
+        self.line = line
+
+    def getLine(self):
+        return self.line
+   
+    def setColumn(self, column):
+        self.column = column
+    
+    def getColumn(self):
+        return self.column
+  
+
+class listOfTokens:
+    # Task 7 - må bruke Token classen på en måte
+    def readFileToTokens(self, file):
         
-        
+        sourceName = file 
+        input = open(sourceName, "r")
+
+        tokens = []
+        for line in input:
+            line = line.rstrip()
+            newLine = re.sub(r"#.*", r"", line)  # Sletter alle kommentarer
+            while newLine!="": # if the new line is different from an empty line
+                if re.match(r"^[ \t]+", newLine):
+                    newLine = re.sub(r"^[ \t]+", "", newLine) # want to match anything but the whitespace and the tabular
+                elif re.match(r"[a-zA-Z_][a-zA-Z0-9_]*", newLine):
+                    identifier = re.match(r"[a-zA-Z_][a-zA-Z0-9_]*", newLine).group()
+                    newLine = re.sub(r"^[a-zA-Z_][a-zA-Z0-9_]*", "", newLine)
+                    # type = 'identifier'
+                    tokens.append(identifier)
+                elif re.match(r'"[^"]*"', newLine):
+                    string = re.match(r'"[^"]*"', newLine).group()
+                    newLine = re.sub(r'"[^"]*"', "", newLine)
+                    tokens.append(string)
+                else:
+                    character = newLine[0]
+                    newLine = newLine[1:]
+                    tokens.append(character)
+
+        input.close()
+
+# Task 9
+class checkCorrectness:
+    # probabilit
+    
+    def validateMarcovChain(self, dtmc, probability_distribution):
+      
+
+
+
+# Task 10
 
 
 
