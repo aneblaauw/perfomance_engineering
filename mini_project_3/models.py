7480f2cbfc48271a4323b581eb54bf935bde5e8b
aneblaauw <aned@mac.com>
2022-04-13 19:21:51 +0200

    small fixes to the classes

--- mini_project_3/models.py
+++ mini_project_3/models.py
@@ -12,6 +12,7 @@
 
 
 from datetime import datetime
+from tkinter import RIGHT
 from numpy import product
 
 """Task 1. Data structures to manage entities involved in the simulation process.
@@ -60,16 +61,35 @@ class Delivery:
         return weight
     
     def add_product(self, product, quantity):
-        pass
-    
+        # TODO: check if delivery already has the product, in that case only increase quantity
+        self.products[product] = quantity
+        
     def remove_product(self, product, quantity):
-        pass
+        # TODO: fix
+        # lower the quantity
+        self.products.remove(product[quantity])
+    
 
 class Robot:
     # can carry products of only one type at a time, but no more than 40kg
-    pass
+    def __init__(self, ) -> None:
+        pass
+
 
 class Cell:
+    # Possible types
+    MOVE = 'M'
+    STORAGE = 'S'
+    LOAD = 'L'
+
+    # Possible directions
+    UP = 'UP'
+    DOWN = 'DOWN'
+    LEFT = 'LEFT'
+    RIGHT = 'RIGHT'
+
+    directions = {UP: 'U', DOWN: 'D', LEFT: 'L', RIGHT: 'R'}
+    
     def  __init__(self, type, x, y, direction =  None, shelves = None) -> None:
         # type = (storage cells, route cells, loading/unloading cells)
         # if route cell, direction is needed
@@ -78,7 +98,38 @@ class Cell:
         self.x = x
         self.y = y
         self.direction = direction # Beste måten å lagre retning på?
-        self.shelves = shelves # an array with shelves, exactly 2 (maybe array is not the best way to store shelves)
+        self.shelves = {} # dictionary with shelves, exactly 2 (maybe array is not the best way to store shelves)
+        # {shelf1: [product1, product2, ..], shelf2: [product1, product2, ...]} Max weight per shelf: ??
+        # TODO: make sure the selves does not overwrite 
+    def addProductsToShelf(self):
+        # {shelf1: [product1, product2, ..], shelf2: [product1, product2, ...]} Max weight per shelf: ??
+        # TODO: make sure the selves does not exeed max weight
+        pass
+    
+    def printCellCord(self):
+        '''
+        prints a cell with x and y coordinate
+
+        [ x, y]
+        '''
+
+        return '[%s,%s]'  % (self.x,  self.y)
+    
+    def printCellType(self):
+        '''
+        prints the cell according to the type
+        move:    [ U ] with the direction î
+        load:    [   ]
+        storage: [ X ]
+
+        '''
+        if self.type == self.MOVE:
+            return '[ %s ]' % self.directions[self.direction]
+        elif self.type == self.LOAD:
+            return '[   ]'
+        else:
+            return '[ X ]'
+
 
 class Shelf:
     def __init__(self, product, quantity) -> None:
@@ -92,6 +143,7 @@ class Warehouse:
         self.floor_map = floor_map # an double linked array of cells, where cells are placed according to coordinates
         self.catalog = catalog
         self.robots = robots # array of robots,maybe not needed in initializing
+        self.client_orders = [] # client orders is added along the way
 
     def add_product(self, product):
         # Assume the product exist
@@ -106,114 +158,17 @@ class Warehouse:
         Remove the product from the warehouse's catalogue
         '''
         if product in self.catalog:
-            self.catalog.products.remove(product)
-        
+            self.catalog.products.remove(product)   
 
     def add_alley(self, size):
-        '''
-        Assumes an alley is a whole row (top to bottom)
-        Adds an alley of given size to the warehouse  floor map
-        An alley will always consist of exactly 2 x size of storage cells, 2x size load/unload cells and 2x move cells
-        This will be the same for top and bottom, and we have to add the middle aisle
-        Middle:
-            - consists of 4 x 6 cells
-        '''
-
-        # Step 2, create the top ailse
-        # TODO: find the coordinates for the middle and end of the warehouse
-        x = None # the coordinates for the end of the warehouse
-        y = None # The coordinates for the middle top
-
-        # coordinates for the first storage cell
-        x = x+1
-        y = y-2
-        for i in range(size):
-            # create the storage cells
-            storage1 = Cell('S',x,y)
-            unload1 = Cell('L', x + 1, y)
-            move1 = Cell('M', x + 2, y, direction='UP')
-
-            
-            move2 = Cell('M', x + 3, y, direction='DOWN')
-            unload2 = Cell('L', x + 4, y)
-            storage2 = Cell('S',x + 5,y)
-
-            # TODO: add the cells to the floor map
-
-            y = y-1
-        
-        # Step 2, create the bottom ailse
-        # TODO: find the coordinates for the middle and end of the warehouse
-        x = None # the coordinates for the end of the warehouse
-        y = None # The coordinates for the middle bottom
-
-        # coordinates for the first storage cell
-        x = x+1
-        y = y+2
-
-        for i in range(size):
-            # create the storage cells
-            storage1 = Cell('S',x,y)
-            unload1 = Cell('L', x + 1, y)
-            move1 = Cell('M', x + 2, y, direction='UP')
-
-            
-            move2 = Cell('M', x + 3, y, direction='DOWN')
-            unload2 = Cell('L', x + 4, y)
-            storage2 = Cell('S',x + 5,y)
-
-            # TODO: add the cells to the floor map
-
-            y = y + 1
-        
-        # Step 4 create the middle
-        # TODO: find the coordinates for the middle and end of the warehouse
-        x = None # the coordinates for the end of the warehouse
-        y = None # The coordinates for the middle top
-
-        x = x+1
-
-        for i in range(2):
-
-            unload1 = Cell('L', x, y-1)
-            unload2 = Cell('L', x +1, y-1)
-            movetop1 = Cell('M', x+2, y-1,  direction='UP')
-            movetop2 = Cell('M', x+3, y-1,  direction='DOWN')
-            unload3 = Cell('L', x+4, y-1)
-            unload4 = Cell('L', x +5, y-1)
-
-            mover1 = Cell('M', x, y, direction='RIGHT')
-            mover2 = Cell('M', x +1, y, direction='RIGHT')
-            mover3 = Cell('M', x +2, y, direction='RIGHT')
-            mover4 = Cell('M', x +3, y, direction='RIGHT')
-            mover5 = Cell('M', x +4, y, direction='RIGHT')
-            mover6 = Cell('M', x +5, y, direction='RIGHT')
-
-            movel1 = Cell('M', x, y +1, direction='LEFT')
-            movel2 = Cell('M', x +1, y +1, direction='LEFT')
-            movel3 = Cell('M', x +2, y +1, direction='LEFT')
-            movel4 = Cell('M', x +3, y +1, direction='LEFT')
-            movel5 = Cell('M', x +4, y +1, direction='LEFT')
-            movel6 = Cell('M', x +5, y +1, direction='LEFt')
-
-            unloadbottom1 = Cell('L', x, y+2)
-            unloadbottom2 = Cell('L', x +1, y+2)
-            movebottom1 = Cell('M', x+2, y+2,  direction='UP')
-            movebottom2 = Cell('M', x+3, y+2,  direction='DOWN')
-            unloadbottom3 = Cell('L', x+4, y+2)
-            unloadbottom4 = Cell('L', x +5, y+2)
-
-            #TODO: add to floor_map
-
-
-
-
-
-
-
-
-        
+        # code in function "addAlley" in utils.py
         pass
+
+    def addClientOrders(self, order):
+        self.client_orders.add(order)
+
+    def addClientOrders(self, order):
+        self.client_orders.remove(order)
         
 class Truck:
     def __init__(self, deliveries) -> None:
@@ -221,7 +176,44 @@ class Truck:
 
 
 class ClientOrder:
-    pass # venter litt
+    def __init__(self, orders) -> None:
+        self.orders = orders # a dictionary on the format {product1: quantity, product2: quantity, ...}
+    
+    def __str__(self) -> str:
+        string = 'Delivery \n created at: %s \n' % self.created_at
+        
+        for product, quantity in self.products.items():
+            string += ' %s – %s stk\n' % (product.sn, quantity)
+        
+        string += 'Total weight: %s kg' % self.get_total_weight()
+        return string
+    
+    
+    def get_total_weight(self):
+        weight = 0
+        for product in self.products.keys():
+            weight += product.weight * self.products[product]
+        
+        return weight
+    
+    def add_product(self, product, quantity):
+        if product in self.orders.keys():
+            self.orders[product] = self.orders[product] + quantity
+        else:
+            self.orders[product] = quantity
+        
+    def remove_product(self, product, quantity):
+        if quantity < self.orders[product]:
+            self.orders[product] = self.orders[product] - quantity
+        if quantity == self.orders[product]:
+            del self.orders[product]
+        else:
+            #TODO: throw error for trying  to removemore products than exists in the order
+            pass
+        
+
+    
+
 
 class Printer:
     """Task3. In charge of printing all info related to the warehouse
--- mini_project_3/simulator.py
+++ mini_project_3/simulator.py
@@ -1,5 +1,9 @@
 # handles the operation of the warehouse
 
-from models import Robot, Cell, Warehouse
+from models import Robot, Cell, Warehouse, ClientOrder
 
 
+
+class Simulator:
+    pass
+    
