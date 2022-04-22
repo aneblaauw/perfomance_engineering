e29af2640b68f07ea73ff79c0ae12b1a76010aea
helenatp <helenatp@stud.ntnu.no>
2022-04-22 11:35:53 +0200

    changes in

---
+++ mini_project_3/robot.py
@@ -0,0 +1,29 @@
+class Robot:
+    # can carry products of only one type at a time, but no more than 40kg
+    WAITING = 'waiting'
+    UNLOAD = 'unload' # should take the product from [0,8] to the storage cell
+    PICKUP = 'pickup' # should take the product from the storage cell to [0,9]
+    
+  
+    def __init__(self, ) -> None:
+        self.currentLocation = [0,8]
+        self.route = None # the route will be created when the robot gets a task
+        self.products = [] # the products the robot carries
+        self.action = self.WAITING
+    
+    def available(self):
+        return (self.route is None)
+    
+    def action(self):
+        #waiting
+        if self.available():
+            return self.WAITING
+        
+        if len(self.products) == 0 and not self.available():
+            return self.PICKUP
+        
+        else:
+            return self.PICKUP
+    
+
+    
